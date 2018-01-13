9fa1c117e64a5503552989d99a0ba6d1bb79c8d8
Aaron Rohrbacher <arjazz@icloud.com>
2018-01-12 23:36:51 -0800

    preview and edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,22 @@ class CommentsController < ApplicationController
     @comment = @post.comments.new
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update!(post_params)
+    redirect_to edit_post_comment_path(@post, @comment)
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(post_params)
+    redirect_to edit_post_comment_path(@post, @comment)
   end
 
   def post_params
