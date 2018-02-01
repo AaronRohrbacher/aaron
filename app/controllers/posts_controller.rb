83c755aa3a2db4c9d7fbbf6770e47d401c4e4db9
Aaron Rohrbacher <arjazz@icloud.com>
2018-01-31 21:43:49 -0800

    unlock site, launch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show]
   def new
     @post = Post.new
   end
