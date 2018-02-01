3467a610846a8b1315623bb21947bc0503905107
Aaron Rohrbacher <arjazz@icloud.com>
2018-01-31 17:27:34 -0800

    temporarily block access while I get a code review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user!
   # def ip_detector(ip_address)
   #   result = RestClient.get("http://api.hostip.info/get_json.php?ip=#{ip_address}")
   #   JSON.parse(result)['city']
