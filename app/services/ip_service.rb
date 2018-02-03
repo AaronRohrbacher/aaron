803270f2d77736629cc300a1b75fa07623e1229a
Aaron Rohrbacher <arjazz@icloud.com>
2018-02-03 00:40:58 -0800

    update to reegeoip API for ip addresses

--- app/services/ip_service.rb
+++ app/services/ip_service.rb
@@ -1,6 +1,11 @@
 class IpService
   def detect_city(ip_address)
-    result = RestClient.get("http://api.hostip.info/get_json.php?ip=#{ip_address}")
+    result = RestClient.get("https://freegeoip.net/json/#{ip_address}")
     JSON.parse(result)['city']
   end
+
+  # def detect_city(ip_address)
+  #   result = RestClient.get("http://api.hostip.info/get_json.php?ip=#{ip_address}")
+  #   JSON.parse(result)['city']
+  # end
 end
