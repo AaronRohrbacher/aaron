5b31e601a5faf6109dd9026c2bd694f1093f7c4a
Aaron Rohrbacher <arjazz@icloud.com>
2018-02-07 21:20:54 -0800

    remove commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!
-  # def ip_detector(ip_address)
-  #   result = RestClient.get("http://api.hostip.info/get_json.php?ip=#{ip_address}")
-  #   JSON.parse(result)['city']
-  # end
 end
