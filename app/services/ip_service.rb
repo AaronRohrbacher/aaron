2192241c648c6e9826452231d619dda40e41f5be
Aaron Rohrbacher <arjazz@icloud.com>
2018-07-15 20:55:19 -0700

    fix ip API query

--- app/services/ip_service.rb
+++ app/services/ip_service.rb
@@ -1,11 +1,7 @@
 class IpService
   def detect_city(ip_address)
-    result = RestClient.get("https://api.ipstack.com/#{ip_address}?access_key=#{ENV['IP_STACK_KEY']}")
+    result = RestClient.get("http://api.ipstack.com/#{ip_address}?access_key=#{ENV['IP_STACK_KEY']}")
+    binding.pry
     JSON.parse(result)['city']
   end
-
-  # def detect_city(ip_address)
-  #   result = RestClient.get("http://api.hostip.info/get_json.php?ip=#{ip_address}")
-  #   JSON.parse(result)['city']
-  # end
 end
