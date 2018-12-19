7afb3b2cbd09ec95213a8316e44af23c3e9fbc2d
Aaron Rohrbacher <arjazz@icloud.com>
2018-12-18 22:13:44 -0800

    adjust IP blocking on comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def not_a_hacker(ip_address_to_screen)
-    if Comment.where("ip_address ILIKE ? AND created_at >= ?", /\b\d{1,3}\.\d{1,3}\.\d{1,3}\b/.match(ip_address_to_screen).to_s + '%', DateTime.now - 1.hour).length >= 5
+    if Comment.where("ip_address ILIKE ? AND created_at >= ?", /\b\d{1,3}\.\d{1,3}\.\d{1,3}\b/.match(ip_address_to_screen).to_s + '%', DateTime.now - 24.hours).length >= 5
       false
     else
       true
